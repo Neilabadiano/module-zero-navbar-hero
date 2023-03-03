7a461a3de33a6aab8e6f4cc9fb5421557e62b969
Neilabadiano <neil.abadiano@module-zero.com>
2023-03-03 19:32:58 +0800

    karons

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,13 +6,13 @@ import { Link } from 'react-router-dom';
 
 
 
-function handleMailto(){
-  const recipient = 'mailto:careers@mytdevcorp.com';
-  const subject = 'Hello';
-  const body = 'How are you doing today?';
-  const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-  window.location.href = mailtoLink;
-}
+// function handleMailto(){
+//   const recipient = 'mailto:careers@mytdevcorp.com';
+//   const subject = 'Hello';
+//   const body = 'How are you doing today?';
+//   const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+//   window.location.href = mailtoLink;
+// }
 
 
 function HeroSection({
@@ -31,8 +31,7 @@ function HeroSection({
     <>
       <div className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}>
         <div className='container'>
-          <div
-            className='row home__hero-row'
+          <div className='row home__hero-row'
             style={{
               display: 'flex',
               flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'
@@ -53,15 +52,9 @@ function HeroSection({
                   {description}
                 </p>
                 <Link to=''>
-{/* 
-                <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=recipient@example.com&su=Email%20Subject&body=Email%20Body">
-  <button>{buttonLabel}</button>
-</a> */}
-
-                <Button buttonSize='btn--wide' onClick={() => window.open("https://mail.google.com/mail/u/0/?view=cm&fs=1&to=careers@mytdevcorp.com&su=Email%20Subject&body=Email%20Body", "_blank")}>
-                  {buttonLabel}
-                </Button>
-
+                  <Button buttonSize='btn--wide' onClick={() => window.open("https://mail.google.com/mail/u/0/?view=cm&fs=1&to=careers@mytdevcorp.com&su=Email%20Subject&body=Email%20Body", "_blank")}>
+                    {buttonLabel}
+                  </Button>
                 </Link>
               </div>
             </div>
@@ -71,10 +64,16 @@ function HeroSection({
                 <img src={img} alt={alt} className='home__hero-img' />
               </div>
             </div>
+
           </div>
         </div>
+
         <div className="hero-shape"></div>
-      </div>
+
+       
+</div>
+
+
     </>
   );
 }
